16347186ec40d2b2d258d885a285019b1f3be6db
HironoriSato <maza.hs5360@gmail.com>
2021-12-16 14:37:05 +0900

    feat: createContextの初期値が不要になるよcreateContextのラップ関数を実装

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,6 +1,18 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import type { RootState, AppDispatch } from './store';
+import { createContext, useContext } from "react";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import type { RootState, AppDispatch } from "./store";
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+// createContextに初期値を渡さないでも警告が出ないようにcreateContextをラップする
+export const createAppContext = <ContextType>() => {
+  const ctx = createContext<ContextType | undefined>(undefined);
+  const useCtx = () => {
+    const c = useContext(ctx);
+    if (!c) throw new Error("useCtx must be inside a Provider with a value");
+    return c;
+  };
+  return [useCtx, ctx.Provider] as const;
+};
