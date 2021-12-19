512d83b11101ecf35c76c3e2d55c4905e6aa547b
Aika Takada <aktsm0syn0pgmail.com>
2021-12-19 15:57:23 +0900

    taskを追加しtasks一覧を表示完了

--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -1,37 +1,72 @@
 import { useState, useEffect } from "react";
 type Task = {
     task: string;
+    readonly id: number;
 };
 interface Task2 {
     task: string;
 }
+// interface inIdTask {
+//     task: string;
+//     id: number;
+// }
 
 export const TodoPage: React.FC = () => {
     const [text, setText] = useState("");
 
     const [tasks, setTasks] = useState<Task[]>([]);
+    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setText(e.target.value);
+    };
     const addTask = () => {
         if (!text) return;
-        const newTask: Task = {
-            task: text
+        const newTask = {
+            task: text,
+            id: new Date().getTime(),
         };
         const oldTasks = tasks.slice();
         oldTasks.unshift(newTask);
         setTasks(oldTasks);
         // setTasks([newTask, ...tasks]); これと同義↑
+        setText("");
 
     };
 
     return (
         <>
-            <form onSubmit={(e) => e.preventDefault()}>
+            <form
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    addTask();
+                }}
+            >
                 <input
                     type="text"
                     value={text}
-                    onChange={(e) => setText(e.target.value)}
+                    onChange={(e) => handleOnChange(e)}
                 />
-                <input type="submit" value="submit" onSubmit={(e) => e.preventDefault} className="submit" />
-                <div>{text}</div>
+                <input
+                    type="submit"
+                    value="submit"
+                    onSubmit={addTask}
+                    className="submit"
+                />
+                <div>
+                    <p>現在のtext state実況中↓</p>
+                    <p>{text}</p>
+                </div>
+                <p>現在のTasksの中身↓</p>
+                <ul>
+                    {tasks.map((task) => {
+
+                        return (
+                            <li key={task.id}>
+                                
+                                <p>{task.task}</p>
+                          </li>
+                        );
+                    })}
+                </ul>
             </form>
         </>
     );
